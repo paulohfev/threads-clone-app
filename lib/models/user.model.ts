ef776d65574b06ab33a0f565b7580e739260af3a
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-08-19 22:04:58 -0300

    Add backend, connection to mongodb and user action

---
+++ lib/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thread'
+    }
+  ],
+  onboarding: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Community'
+    }
+  ]
+});
+
+// fallbacks to creating a mongoose model User if it doesn't exist.
+// for the second, the user model is already created
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
